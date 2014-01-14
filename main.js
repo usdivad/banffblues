da615c4854218948c6f871a3b3385100881924a9
David Su <david.d.su@gmail.com>
2014-01-14 07:40:22 -0500

    metro sound on

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 var x = new Engine();
-x.playChord([68,70,72,74,76,78]);
+var b = new Bar();
+b.playBeat(x);
+//x.playChord([68,70,72,74,76,78]);
--- playback.js
+++ playback.js
@@ -14,17 +14,36 @@ function Engine() {
 	//IVs for bars
 	t.barList = [];
 	t.barList.push(new Bar());
-	t.currentBar = barList[0];
+	t.currentBarIndex = 0;
 
 	//IVs for synthesis
-	t.osc = T("tri");
+	t.osc = T("pulse");
 	t.env = T("adsr", {a:1, d:200, s:0.25, r:500});
 	t.oe = T("OscGen", {osc:t.osc, env:t.env, mul:1}).play();
+	t.met = T("fnoise", {freq:512, mul:0}).play();
 
 	//IVs for timing
 	t.bpm = 120; 
 	t.timer = T("interval", {interval:t.bpm}, function() {
 
+		//playBeat() + det new bars
+		if (t.barList[currentBarIndex].done()) {
+			//check whether we have more bars
+			if (currentBarIndex <= t.barList.length) {
+				currentBarIndex++;
+				t.barList[currentBarIndex].playBeat(t);
+			}
+			else {
+				currentBarIndex = -1;
+				//stopAll();
+			}
+		}
+		else {
+			t.barList[currentBarIndex].playBeat(t);
+		}
+
+
+		//CSS work
 	});
 
 	//Plays a chord given as array of MIDI pitches (only one articulation so far)
@@ -41,6 +60,23 @@ function Engine() {
 		setTimeout(function() {
 			t.oe.allNoteOff();
 		}, 1000); //1000ms
+	}
+
+	//Plays metronome beats
+	t.playMetro = function(beat) {
+		if (beat === "downbeat") {
+			t.met.freq = 3000; //in hz
+		}
+		else if (beat === "upbeat") {
+			t.met.freq = 1000; 
+		}
+
+		t.met.mul = 0.1;
+
+		setTimeout(function() {
+			t.met.mul = 0;
+		}, 100); //100ms
+
 	}
 }
 
