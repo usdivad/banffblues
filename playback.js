df500f730b0aca4dd249778f9f9fc3b5fbd01138
David Su <david.d.su@gmail.com>
2014-01-14 06:40:54 -0500

    houston we have sound

--- playback.js
+++ playback.js
@@ -1,4 +1,31 @@
-function den_to_ms(den, bpm) {
+/*
+dependencies:
+- timbre.js
+*/
+
+function Engine() {
+	var t = this;
+	t.osc = T("tri");
+	t.env = T("adsr", {a:1, d:200, s:0.25, r:500});
+	t.oe = T("OscGen", {osc:t.osc, env:t.env, mul:1}).play();
+
+	t.playNote = function(noteArray) {
+		var velocity = 25;
+		for (var i=0; i<noteArray.length; i++) {
+			var note = noteArray[i];
+			t.oe.noteOn(note, velocity);
+			//get the ind noteOff to work
+			/*setTimeout(function(note) {
+				t.oe.noteOff(note);
+			}, 500);*/
+		}
+		setTimeout(function() {
+			t.oe.allNoteOff();
+		}, 1000); //500ms
+	}
+}
+
+function denToMs(den, bpm) {
 	var q_ms = (60/bpm)*1000;
 	return q_ms*(4/den);
 }
